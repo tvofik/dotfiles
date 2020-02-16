2a3e36d3359678b730f42a36fcf766973e4dd039
Abayomi Pelewura <tvofik@Abayomis-MBP.fios-router.home>
2020-02-16 00:43:31 -0500

    Initial Commit

---
+++ brew.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Install brew packages
+brew install tree
+brew install python
+brew install awscli
+brew install terraform
+brew install docker
+brew install ansible
+brew install tldr
+brew install jq
+brew install git-secrets
+brew install git
+brew install bash-completion
+
+# Jokes
+brew install cowsay
+brew install htop
+brew install tmux
+
+# Install MacOS Applications
+brew cask install google-chrome
+brew cask install firefox-developer-edition
+brew cask install visual-studio-code
+brew cask install spotify
+brew cask install discord
+brew cask install vlc
+brew cask install numi
+brew cask install slack
+brew cask install iterm2
+brew cask install appcleaner
+brew cask install postman
+brew cask install balenaetcher
+brew cask install notion
+brew cask install dashlane
+brew cask install teamviewer
+brew cask install maccy
+
+# brew cask install vmware-fusion
+# brew cask install processing
+# brew cask install dash
+# brew cask install sublime-text
+# brew cask install virtualbox
+# brew cask install vmware-fusion
+# brew cask install sourcetree
+# brew cask install google-backup-and-sync
+# brew cask install skype
+# brew cask install gimp
+# brew cask install hyperdock
+# brew cask install divvy
---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+############################
+# This script creates symlinks from the home directory to any desired dotfiles in ${homedir}/dotfiles
+# And also installs Homebrew Packages
+############################
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: install.sh <home_directory>"
+    exit 1
+fi
+
+homedir=$1
+
+# dotfiles directory
+dotfiledir=${homedir}/dotfiles
+
+# list of files/folders to symlink in ${homedir}
+files="bash_profile bash_prompt aliases gitconfig"
+
+# change to the dotfiles directory
+echo "Changing to the ${dotfiledir} directory"
+cd ${dotfiledir}
+echo "...done"
+
+# create symlinks (will overwrite old dotfiles)
+for file in ${files}; do
+    echo "Creating symlink to $file in home directory."
+    ln -sf ${dotfiledir}/.${file} ${homedir}/.${file}
+done
+
+# Download Git Auto-Completion
+curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash" > ${homedir}/.git-completion.bash
+curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh" > ${homedir}/.git-prompt.sh
+
+# Run the Homebrew Script
+./brew.sh
+
+# Download Docker Completion
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.25.3/contrib/completion/bash/docker-compose -o /usr/local/etc/bash_completion.d/docker-compose
+
+# Run the pip script
+./pip.sh
