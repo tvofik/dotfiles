2476a56cda82381aa25233012a474bde93f79720
Abayomi Pelewura <abayomi_pelewura@yahoo.co.uk>
2020-03-02 18:28:24 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: install.sh <home_directory>"
+    exit 1
+fi
+
+homedir=$1
+
+echo "Installing a new machine? Nice!"
+
+echo ${homedir}
+
+if ! command -v brew > /dev/null 2>&1; then
+  echo "Installing homebrew..."
+#  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+  echo "Homebrew is already installed... skipping the installation"
+  echo "Updating Homebrew..."
+#  brew update
+fi
+
+if ! command -v git > /dev/null 2>&1 ; then
+  echo "Installing Git..."
+#  brew install git
+else
+  echo "Upgrading Git..."
+#  brew upgrade git
+fi
+
+# Clone Repo 
+# git clone https://github.com/tvofik/dotfiles.git ~/dotfiles
+# cd ~/dotfiles
+# . ./install.sh
