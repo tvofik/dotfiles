b7322bd00c549d93bf227be83d7f51cd52af65e8
Abayomi Pelewura <abayomi_pelewura@yahoo.co.uk>
2020-03-02 18:22:36 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,17 +11,6 @@ fi
 
 homedir=$1
 
-echo "Installing a new machine? Nice!"
-
-if ! command -v brew > /dev/null 2>&1; then
-  echo "Okay, lets do this. First things first, we need to install homebrew in order to get all the stuff you need"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-else
-  echo "Homebrew was already available... skipping the installation"
-  echo "Let's update it instead! (you know you haven't updated in a while)"
-  brew update
-fi
-
 # dotfiles directory
 dotfiledir=${homedir}/dotfiles
 
