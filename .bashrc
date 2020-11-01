f8f60be5a1d3c31a8e869bcdb23f62e46d8895b0
Abayomi Pelewura <abayomi_pelewura@yahoo.co.uk>
2020-11-01 01:27:33 -0400

    Add zshrc & remove bashrc

--- .bashrc
+++
@@ -1,4 +0,0 @@
-# If not running interactively, don't do anything
-[[ $- == *i* ]] || return
-
-[ -n "$PS1" ] && source ~/.bash_profile;
--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ homedir=$1
 dotfiledir=${homedir}/dotfiles
 
 # list of files/folders to symlink in ${homedir}
-files="bash_profile bash_prompt aliases gitconfig"
+files="aliases gitconfig"
 
 # change to the dotfiles directory
 echo "Changing to the ${dotfiledir} directory"
@@ -28,15 +28,8 @@ for file in ${files}; do
     ln -sf ${dotfiledir}/.${file} ${homedir}/.${file}
 done
 
-# Download Git Auto-Completion
-curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash" > ${homedir}/.git-completion.bash
-curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh" > ${homedir}/.git-prompt.sh
-
 # Run the Homebrew Script
 . ./brew.sh
-
-# Download Docker Completion
-sudo curl -L https://raw.githubusercontent.com/docker/compose/1.25.3/contrib/completion/bash/docker-compose -o /usr/local/etc/bash_completion.d/docker-compose
 
 # Run the pip script
 . ./pip.sh
@@ -48,6 +41,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # ITERM Preference setup
 # Specify the preferences directory
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm"
+
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
