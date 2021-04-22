452e134c81399434e270ddeb27e104d33b9fd416
Abayomi Pelewura <17411689+tvofik@users.noreply.github.com>
2021-04-22 17:49:23 -0400

    Configuring OS

--- install.sh
+++ install.sh
@@ -53,3 +53,16 @@ defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
 # Speed up the Dock animation time
 defaults write com.apple.dock autohide-time-modifier -float 0.25;killall Dock
+
+
+# echo "Configuring OS..."
+# # Set fast key repeat rate
+# defaults write NSGlobalDomain KeyRepeat -int 0
+# # Require password as soon as screensaver or sleep mode starts
+# defaults write com.apple.screensaver askForPassword -int 1
+# defaults write com.apple.screensaver askForPasswordDelay -int 0
+# # Show filename extensions by default
+# defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+# # Enable tap-to-click
+# defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+# defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
