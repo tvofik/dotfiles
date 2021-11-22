d8eee735e239af108b251705f77c50c6931fec3f
Abayomi Pelewura <17411689+tvofik@users.noreply.github.com>
2021-11-22 17:41:16 -0500

    setup changed

--- setup.sh
+++ setup.sh
@@ -9,30 +9,30 @@
 
 # echo "Setting up a machine? Nice!"
 
-echo ${homedir}
+# echo ${homedir}
 
-if ! command -v brew > /dev/null 2>&1; then
+if ! [[ command -v brew > /dev/null 2>&1 ]]; then
   echo "Installing homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  # Add Brew to PATH for arm64 Architecture "M1 Mac" &  Install Rosetta
   if [[ arch = "arm64" ]]; then 
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/tvofik/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
     sudo softwareupdate --install-rosetta --agree-to-license
-  fi
-    # Add brew to PATH for arm64 Architecture "M1 macs" 
     # echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/tvofik/.zprofile
     # eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
 else
   echo "Homebrew is already installed... skipping the installation"
   echo "Updating Homebrew..."
-  brew update    
+  brew update
 fi
 
 # Clone Repo
 echo "Cloning Repo..."
 git clone https://github.com/tvofik/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
+
 # /bin/bash install.sh ${homedir}
-echo $HOME is the Home Dir 
+echo "$HOME is the HOME directory"
 /bin/bash install.sh $HOME
-
-
