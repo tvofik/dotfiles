1d5ff33eb7e40b1d82977106ebe098b957e60908
Abayomi Pelewura <17411689+tvofik@users.noreply.github.com>
2021-11-23 10:47:11 -0500

    Icons to dock

--- sysconfig.sh
+++ sysconfig.sh
@@ -57,3 +57,7 @@ defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
 # links 
 # https://www.intego.com/mac-security-blog/unlock-the-macos-docks-hidden-secrets-in-terminal/
+
+
+#Add the application to dock Tested and works
+# defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Dash.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"; killall Dock
