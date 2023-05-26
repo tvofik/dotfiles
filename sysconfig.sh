150e076cb582362cb65191d1eaa47ac047c3366b
Abayomi Pelewura <17411689+tvofik@users.noreply.github.com>
2023-05-26 18:25:14 -0400

    Update sysconfig.sh

--- sysconfig.sh
+++ sysconfig.sh
@@ -14,7 +14,6 @@ killall Dock #Restart the Dock
 # ITERM Preference setup
 # Specify the preferences directory
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm"
-
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
